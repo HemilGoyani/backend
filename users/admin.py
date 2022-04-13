fdcadeff728c72ce1f16a64a798f1691d5de51e5
hemil goyani <goyanihemil8@gmail.com>
2022-04-13 09:24:12 +0530

    admin file changes to the display list field add

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from users.models import User
 
 class UserModel(admin.ModelAdmin):
-    list_display = ('username', 'email', 'date_joined', 'is_staff', 'is_active', 'is_superuser')
+    list_display = ('id','username', 'email', 'date_joined', 'is_staff', 'is_active', 'is_superuser')
     
 admin.site.register(User, UserModel)
